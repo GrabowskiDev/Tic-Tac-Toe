6f2096e3a5815720c0d106906f33f47b439da518
GrabowskiDev <lukaszgra2004@gmail.com>
2021-05-08 20:27:24 +0200

    Rearrange code and add mark to array

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
 const gameBoard = (() => {
-    let array = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-    let mark = " ";
-
     //Query Selectors
-    const mainBoard = document.querySelector('.gameBoard');
     const turnInfo = document.querySelector('.turnInfo');
 
     //Populating display with content of array
-    const populate = () => {
+    const populate = (array) => {
         for(i=0; i<9; i++) {
             document.querySelector(`div[data-index="${i}"]`).textContent = array[i];
         }
     };
 
-    //Add mark to selected div
-    const addMark = index => {
-        if(array[index]==" ") array[index] = mark;
-    }
-
-    //Listens to clicks on mainBoard divs
-    mainBoard.childNodes.forEach((div) => {
-        div.addEventListener(('click'), () => {
-            //Calls addMark func with clicked div index
-            addMark(div.getAttribute('data-index'));
-            populate();
-        });
-    });
-
-    //Set turn info to indicate who's turn it is
+        //Set turn info to indicate who's turn it is
     const setTurnInfo = player => {
         turnInfo.textContent = `It's ${player}'s turn`;
+    }
+    
+    return {
+        populate,
+        setTurnInfo,
+    };
+})();
+
+const game = (() => {
+    let array = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    let rounds = 0;
+    let mark = "X";
+
+    //Query Selectors
+    const mainBoard = document.querySelector('.gameBoard');
+    
+    const startGame = () => {
+        //Player with X starts the game
+        playerX.turn();
+    };
+    
+    //Add mark to selected div
+    const addMark = index => {
+        if(array[index]==" ") { 
+            array[index] = mark;
+            gameBoard.populate(array);
+            nextRound();
+        }
     }
     
     //Clears the array
     const reset = () => {
         array = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
     }
-
-    return {
-        setTurnInfo,
-        reset,
-        mark
-    };
-})();
-
-const game = (() => {
-    const startGame = () => {
-        //Player with X starts the game
-        playerX.turn();
+    
+    const nextRound = () => {
+        ++rounds;
+        if(rounds%2===1) {
+            playerO.turn();
+            mark = "O";
+        } else {
+            playerX.turn();
+            mark = "X";
+        }
     };
 
-    return {
+    //Listens to clicks on mainBoard divs
+    mainBoard.childNodes.forEach((div) => {
+        div.addEventListener(('click'), () => {
+            //Calls addMark func with clicked div index
+            addMark(div.getAttribute('data-index'));
+            
+        });
+    });
 
+    return {
+        startGame,
     }
 })();
 
 const playerFactory = (name, mark) => {
-    let name = name;
-    let mark = mark;
-
     const turn = () => {
         gameBoard.setTurnInfo(name);
     }
-    return {
 
+    return {
+        turn,
     }
 };
 
 const playerX = playerFactory("Jakub", "X");
 const playerO = playerFactory("Bartek", "O");
+
+game.startGame();
