06271396a4bed468799f889d7813a753f6d6e07f
GrabowskiDev <lukaszgra2004@gmail.com>
2021-05-08 10:17:25 +0200

    add modules and player factory function

---
+++ script.js
@@ -0,0 +1,21 @@
+const gameBoard = (() => {
+    let array = ["", "", "", "", "", "", "", "", ""];
+    
+    return {
+
+    };
+})();
+
+const displayController = (() => {
+
+    return {
+        
+    }
+})();
+
+const playerFactory = name => {
+
+    return {
+
+    }
+};
